11cad2803fd50f22d772edb65e5fa49e1b2ea4c6
minhaz74692 <minhaz74692@gmail.com>
2022-07-24 22:57:24 +0600

    Little  change in notes

--- client/src/componants/Notes.js
+++ client/src/componants/Notes.js
@@ -45,7 +45,7 @@ const Notes = (props) => {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 className="modal-title text-center" id="exampleModalLabel">Update note</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
