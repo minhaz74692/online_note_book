9c6184990749b4f171414f84d44ca736e81a3aa1
minhaz74692 <minhaz74692@gmail.com>
2022-07-27 21:34:04 +0600

    Change in about component

--- client/src/componants/About.js
+++ client/src/componants/About.js
@@ -4,7 +4,7 @@ const About=()=> {
     <div>
     <h2 className='text-center'>About My Note Book</h2>
     <p className='p-3 col-lg-8 d-block mx-auto fs-5'>
-      <strong>"My Note Book"</strong> is a MERN Application. It help you to store your notes on cloud. You can store your note with lifetime access. All the datas will securely store in mongoDB Atlas. You can also modify and delete note at any time.
+      <strong>"My Note Book"</strong> is a MERN Application. It helps you to store your notes on MongoDB cloud. You can store your notes with lifetime access. All the datas will securely store in mongoDB Atlas. You can also modify and delete notes at any time.
     </p>
     </div>
   )
